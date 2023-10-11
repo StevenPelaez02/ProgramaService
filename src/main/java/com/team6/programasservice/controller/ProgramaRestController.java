77ab2dbea06a4c00698cb020173ddf4e34ed5852
Juan Gutierrez <juanko6@gmail.com>
2023-10-11 11:01:04 -0500

    se crea en ProgramaRestControllert crear programa con endpoint, se crea interface programa, se crea modelo programa , se crea metodo crear en service, se dejan TODO para nuevas implementaciones

--- src/main/java/com/team6/programasservice/controller/ProgramaRestController.java
+++ src/main/java/com/team6/programasservice/controller/ProgramaRestController.java
@@ -1,4 +1,23 @@
 package com.team6.programasservice.controller;
 
+import com.team6.programasservice.model.entities.Programa;
+import com.team6.programasservice.model.service.IProgramaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/programa-service")
 public class ProgramaRestController {
+    @Autowired
+    private IProgramaService programaService;
+
+    @PostMapping("/programa")
+    public Programa crearPrograma(@RequestBody Programa programa){
+        return programaService.save(programa);
+    }
+
+    //TODO crear ENDPOINTS e implementar metodos de listar, actualizar y eliminar
 }
--- src/main/java/com/team6/programasservice/model/service/ProgramaServiceImpl.java
+++ src/main/java/com/team6/programasservice/model/service/ProgramaServiceImpl.java
@@ -1,4 +1,20 @@
 package com.team6.programasservice.model.service;
 
-public class ProgramaServiceImpl {
+import com.team6.programasservice.model.dao.IProgramaDao;
+import com.team6.programasservice.model.entities.Programa;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProgramaServiceImpl implements IProgramaService{
+    @Autowired
+    IProgramaDao programaDao;
+
+
+    @Override
+    public Programa save(Programa programa) {
+        return programaDao.save(programa);
+    }
+
+    //TODO sobreescribir metodos de la interface de listar, buscar y eliminar
 }
